9810c0392e247d4d0d483693b0f3a5fa57b39abe
Thomas Fredriksen <thomas@kaldpost.net>
2019-05-23 22:14:29 +0200

    Moved data from main cachetur repository

---
+++ cacheturhelper.meta.js
@@ -0,0 +1,34 @@
+﻿// ==UserScript==
+// @name            The Cachetur Assistant
+// @name:no         Cacheturassistenten
+// @author          cachetur.no, thomfre
+// @namespace       http://cachetur.no/
+// @version         3.1.4.1
+// @description     Companion script for cachetur.no
+// @description:no  Hjelper deg å legge til cacher i cachetur.no
+// @icon            https://cachetur.net/img/logo_top.png
+// @include         https://www.geocaching.com/map/*
+// @include         http://www.geocaching.com/map/*
+// @include         https://www.geocaching.com/geocache/*
+// @include         http://www.geocaching.com/geocache/*
+// @include         https://www.geocaching.com/seek/cache_details.aspx*
+// @include         https://www.geocaching.com/bookmarks/view.aspx*
+// @include         http://www.geocaching.com/play/geotours/*
+// @include         https://www.geocaching.com/play/geotours/*
+// @include         http://project-gc.com/*
+// @include         https://project-gc.com/*
+// @connect         cachetur.no
+// @connect         cachetur.net
+// @connect         self
+// @grant           GM_xmlhttpRequest
+// @grant           GM_info
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @grant           GM_openInTab
+// @grant           GM_addStyle
+// @grant           unsafeWindow
+// @run-at          document-start
+// @copyright       2017+, cachetur.no
+// @require         https://code.jquery.com/jquery-latest.js
+// @downloadURL     https://cachetur.no/monkey/cacheturhelper.user.js
+// ==/UserScript==
